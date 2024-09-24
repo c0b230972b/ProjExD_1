a8f5b1832d58cd2f07b0fc87a06ab93e00d69d71
c0b23097 <c0b230972b@edu.teu.ac.jp>
2024-09-24 15:27:55 +0900

    練習問題2　こうかとんの読み込み、左右反転

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png") #練習問題2
+    kk_img = pg.transform.flip(kk_img, True, False) #引数(画像Surface,左右反転,上下反転)
     tmr = 0
     while True:
         for event in pg.event.get():
