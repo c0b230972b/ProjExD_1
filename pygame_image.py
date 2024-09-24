3501350f49bc82b5a1e2c2607776124992e742f4
c0b23097 <c0b230972b@edu.teu.ac.jp>
2024-09-24 16:24:30 +0900

    練習問題8　こうかとん自由自在

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,29 @@ def main():
     bg_img2 = pg.transform.flip(bg_img2,True, False)
     kk_img = pg.image.load("fig/3.png") #練習問題2 こうかとん画像読み込み
     kk_img = pg.transform.flip(kk_img, True, False) #引数(画像Surface,左右反転,上下反転)
+    kk_rct = kk_img.get_rect() #練習問題8-1 SurfaceからRectを抽出する
+    kk_rct.center = 300, 200 #練習問題8-2 初期座標を設定
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
+        key_lst = pg.key.get_pressed() #練習問題8-3 key方向
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1)) #(横方向速度, 縦方向速度)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
 
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2,[x+1600, 0])
-        screen.blit(bg_img, [x+3200, 0])
-        screen.blit(kk_img, [300, 200]) #練習問題4 こうかとん描画
+        screen.blit(bg_img, [x+3200, 0])#練習問題8-5 描画
+        screen.blit(kk_img, kk_rct) #練習問題4 こうかとん描画
         
         pg.display.update()
         tmr += 1        
