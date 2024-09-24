8aa75d0d01cb9506b9bf92e0b6ce5ff0470b1c58
c0b23097 <c0b230972b@edu.teu.ac.jp>
2024-09-24 15:33:27 +0900

    練習問題5　FPS変更

--- pygame_image.py
+++ pygame_image.py
@@ -18,10 +18,10 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200]) #練習問題3 こうかとん描画
+        screen.blit(kk_img, [300, 200]) #練習問題4 こうかとん描画
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200) #練習問題5 FPSを200に変更
 
 
 if __name__ == "__main__":
