a456a782cf38788c9cd50f480565454faccc8f30
c0b23097 <c0b230972b@edu.teu.ac.jp>
2024-09-24 16:04:54 +0900

    練習問題7 背景画像ワープ解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img2,True, False)
     kk_img = pg.image.load("fig/3.png") #練習問題2 こうかとん画像読み込み
     kk_img = pg.transform.flip(kk_img, True, False) #引数(画像Surface,左右反転,上下反転)
     tmr = 0
@@ -17,9 +19,12 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = -(tmr%800)
+        x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2,[x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
         screen.blit(kk_img, [300, 200]) #練習問題4 こうかとん描画
+        
         pg.display.update()
         tmr += 1        
         clock.tick(200) #練習問題5 FPSを200に変更
